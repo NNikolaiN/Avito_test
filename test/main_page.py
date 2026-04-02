fc9b3a30109ee77bba1f6e70660a78beb902cf28
NNikolaiN <n95237706@gmail.com>
2026-04-02 23:04:53 +0300

    Создание теста сортировки по цене

--- test/main_page.py
+++ test/main_page.py
@@ -6,8 +6,9 @@ class MainPage():
         
         self.min_price_input: Locator = page.get_by_placeholder("От")
         self.max_price_input: Locator = page.get_by_placeholder("До")
-        self.filter_select: Locator = page.locator("_filters__select_1iunh_21")
-        self.sorted_select: Locator = page.locator("_filters__select_1iunh_21")
+        self.category_select: Locator = page.locator("label._filters__label_1iunh_8:has-text('Категория') + select._filters__select_1iunh_21")
+        self.sorting_select: Locator = page.locator("label._filters__label_1iunh_8:has-text('Сортировать по') + select._filters__select_1iunh_21")
+        self.order_select: Locator = page.locator("label._filters__label_1iunh_8:has-text('Порядок') + select._filters__select_1iunh_21")
         self.urgent_toggle: Locator = page.get_by_role('checkbox')
         
         self.announcement_cards: Locator = page.locator(".card_15fhn_2")
@@ -20,16 +21,33 @@ class MainPage():
         self.min_price_input.fill(str(min_price))
         self.max_price_input.fill(str(max_price))
         
+        self.price_elements.first.wait_for(state="visible", timeout=12000)
+    
+    def sort_by_price(self, order: str = "asc"):    
+        self.sorting_select.select_option(label="Цене")
+        
+        if order.lower == "asc":
+            self.order_select.select_option(label='По возрастанию')
+        else:
+            self.order_select.select_option(label='По убыванию')
+            
+        self.price_elements.first.wait_for(state="visible", timeout=12000)
+        
     def toggle_urgent_only(self, enable:bool):
         pass
     
     def get_price_of_items(self):
-        self.price_elements.first.wait_for(state="visible", timeout=10000)
-        prices = self.price_elements.all_text_contents()
+        try:
+            self.price_elements.first.wait_for(state="visible", timeout=8000)
+        except:
+            self.announcement_cards.first.wait_for(state="visible", timeout=8000)
+        
+        texts = self.price_elements.all_text_contents()
         
         list_price = []
+        for text in texts:
+            clean = ''.join(filter(str.isdigit, text.replace(" ", "").replace("₽", "")))
+            if clean:
+                list_price.append(float(clean))
         
-        for i in prices:
-            clean_price = ''.join(filter(str.isdigit, i.replace(" ", "").replace("₽", "")))
-            list_price.append(float(clean_price))
         return list_price
--- test/test_main_page.py
+++ test/test_main_page.py
@@ -11,5 +11,14 @@ def test_filter_price(page):
     prices = main_page.get_price_of_items()
     
     assert len(prices) > 0
-    assert all(1000 <= p <= 3000 for p in prices)
+    assert all(1000 <= p <= 3000 for p in prices), "Фильтр цены не работает"
 
+# Тестироание сортировки "По цене"
+def test_sort_by_price(page):
+    main_page = MainPage(page)
+    main_page.goto()
+    
+    main_page.sort_by_price()
+    price = main_page.get_price_of_items()
+    
+    assert price == sorted(price, reverse=True), "Сортировка по цене не работает корректно"
